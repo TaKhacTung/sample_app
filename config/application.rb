953eb5406f0d0a6b903f685f3151d010502e0c5d
TaKhacTung <k.tung130697@gmail.com>
2019-07-17 19:58:14 +0700

    Chapter 3: Mostly static pages

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module SampleApp
   class Application < Rails::Application
     
     config.load_defaults 5.2
-
-    
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
     config.generators.system_tests = nil
   end
 end
